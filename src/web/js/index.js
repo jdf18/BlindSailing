12ecf0053d96c2c67115d882e8b0bd013c52cd7b
jdf18 <97624626+jdf18@users.noreply.github.com>
2024-11-02 21:22:26 +0000

    Join lobby button on index page

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -3,3 +3,8 @@ async function create_lobby() {
     const lobby_uid = await api_create_lobby();
     window.location.replace("/lobby/"+lobby_uid);
 }
+
+function join_lobby() {
+    const lobby_uid = document.getElementById('lobby-code').value;
+    window.location.replace("/lobby/"+lobby_uid);
+}
