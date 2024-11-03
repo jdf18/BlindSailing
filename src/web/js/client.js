7995e72bd51a0e4157555525dcbb6b1324b5bbe7
HarryRich02 <klvr36@durham.ac.uk>
2024-11-03 05:45:32 +0000

    added more images, going insane from sleep deprivation

--- src/web/js/client.js
+++ src/web/js/client.js
@@ -1,25 +1,86 @@
 let client_turn_interval = null;
 let wait_for_opponent_interval = null;
-const airCarrier_0 = new Image();
-airCarrier_0.src = "/assets/airCarrier-0.png";
-const airCarrier_1 = new Image();
-airCarrier_1.src = "/assets/airCarrier-1.png";
-const battleship_0 = new Image();
-battleship_0.src = "/assets/battleship-0.png";
-const battleship_1 = new Image();
-battleship_1.src = "/assets/battleship-1.png";
-const cruiser_0 = new Image();
-cruiser_0.src = "/assets/cruiser-0.png";
-const cruiser_1 = new Image();
-cruiser_1.src = "/assets/cruiser-1.png";
-const destroyer_0 = new Image();
-destroyer_0.src = "/assets/destroyer-0.png";
-const destroyer_1 = new Image();
-destroyer_1.src = "/assets/destroyer-1.png";
-const submarine_0 = new Image();
-submarine_0.src = "/assets/submarine-0.png";
-const submarine_1 = new Image();
-submarine_1.src = "/assets/submarine-1.png";
+const airCarrier_0_N = new Image();
+airCarrier_0_N.src = "/assets/n-airCarrier-0.png";
+const airCarrier_0_S = new Image();
+airCarrier_0_S.src = "/assets/s-airCarrier-0.png";
+const airCarrier_0_E = new Image();
+airCarrier_0_E.src = "/assets/e-airCarrier-0.png";
+const airCarrier_0_W = new Image();
+airCarrier_0_W.src = "/assets/w-airCarrier-0.png";
+const airCarrier_1_N = new Image();
+airCarrier_1_N.src = "/assets/n-airCarrier-1.png";
+const airCarrier_1_S = new Image();
+airCarrier_1_S.src = "/assets/s-airCarrier-1.png";
+const airCarrier_1_E = new Image();
+airCarrier_1_E.src = "/assets/e-airCarrier-1.png";
+const airCarrier_1_W = new Image();
+airCarrier_1_W.src = "/assets/w-airCarrier-1.png";
+const battleship_0_N = new Image();
+battleship_0_N.src = "/assets/n-battleship-0.png";
+const battleship_0_S = new Image();
+battleship_0_S.src = "/assets/s-battleship-0.png";
+const battleship_0_E = new Image();
+battleship_0_E.src = "/assets/e-battleship-0.png";
+const battleship_0_W = new Image();
+battleship_0_W.src = "/assets/w-battleship-0.png";
+const battleship_1_N = new Image();
+battleship_1_N.src = "/assets/n-battleship-1.png";
+const battleship_1_S = new Image();
+battleship_1_S.src = "/assets/s-battleship-1.png";
+const battleship_1_E = new Image();
+battleship_1_E.src = "/assets/e-battleship-1.png";
+const battleship_1_W = new Image();
+battleship_1_W.src = "/assets/w-battleship-1.png";
+const cruiser_0_N = new Image();
+cruiser_0_N.src = "/assets/n-cruiser-0.png";
+const cruiser_0_S = new Image();
+cruiser_0_S.src = "/assets/s-cruiser-0.png";
+const cruiser_0_E = new Image();
+cruiser_0_E.src = "/assets/e-cruiser-0.png";
+const cruiser_0_W = new Image();
+cruiser_0_W.src = "/assets/w-cruiser-0.png";
+const cruiser_1_N = new Image();
+cruiser_1_N.src = "/assets/n-cruiser-1.png";
+const cruiser_1_S = new Image();
+cruiser_1_S.src = "/assets/s-cruiser-1.png";
+const cruiser_1_E = new Image();
+cruiser_1_E.src = "/assets/e-cruiser-1.png";
+const cruiser_1_W = new Image();
+cruiser_1_W.src = "/assets/w-cruiser-1.png";
+const destroyer_0_N = new Image();
+destroyer_0_N.src = "/assets/n-destroyer-0.png";
+const destroyer_0_S = new Image();
+destroyer_0_S.src = "/assets/s-destroyer-0.png";
+const destroyer_0_E = new Image();
+destroyer_0_E.src = "/assets/e-destroyer-0.png";
+const destroyer_0_W = new Image();
+destroyer_0_W.src = "/assets/w-destroyer-0.png";
+const destroyer_1_N = new Image();
+destroyer_1_N.src = "/assets/n-destroyer-1.png";
+const destroyer_1_S = new Image();
+destroyer_1_S.src = "/assets/s-destroyer-1.png";
+const destroyer_1_E = new Image();
+destroyer_1_E.src = "/assets/e-destroyer-1.png";
+const destroyer_1_W = new Image();
+destroyer_1_W.src = "/assets/w-destroyer-1.png";
+const submarine_0_N = new Image();
+submarine_0_N.src = "/assets/n-submarine-0.png";
+const submarine_0_S = new Image();
+submarine_0_S.src = "/assets/s-submarine-0.png";
+const submarine_0_E = new Image();
+submarine_0_E.src = "/assets/e-submarine-0.png";
+const submarine_0_W = new Image();
+submarine_0_W.src = "/assets/w-submarine-0.png";
+const submarine_1_N = new Image();
+submarine_1_N.src = "/assets/n-submarine-1.png";
+const submarine_1_S = new Image();
+submarine_1_S.src = "/assets/s-submarine-1.png";
+const submarine_1_E = new Image();
+submarine_1_E.src = "/assets/e-submarine-1.png";
+const submarine_1_W = new Image();
+submarine_1_W.src = "/assets/w-submarine-1.png";
+
 const explosion = new Image();
 explosion.src = "/assets/explosion.png";
 const fog = new Image();
@@ -105,7 +166,7 @@ async function render() {
     const enemyShips = await api_get_visible_enemy_ships();
     for (let i=0; i<enemyShips.length; i++) {
         let coord = coord_to_pos(enemyShips[i][1], enemyShips[i][2]);
-        ctx.drawImage(get_img(enemyShips[i][0]), coord[0], coord[1]);
+        ctx.drawImage(get_img(enemyShips[i][0], enemyShips[i][2]), coord[0], coord[1]);
     };
 
     const hiddenCells = await api_get_hidden_cells();
@@ -117,7 +178,7 @@ async function render() {
     const friendlyShips = await api_get_visible_friendly_ships();
     for (let i=0; i<friendlyShips.length; i++) {
         let coord = coord_to_pos(friendlyShips[i][1], friendlyShips[i][2]);
-        ctx.drawImage(get_img(friendlyShips[i][0]), coord[0], coord[1]);
+        ctx.drawImage(get_img(friendlyShips[i][0], friendlyShips[i][2]), coord[0], coord[1]);
     };
 
     const damagedCells = await api_get_damaged_squares();
@@ -127,61 +188,130 @@ async function render() {
     };
 };
 
-function define_images() {
-    const airCarrier_0 = new Image();
-    airCarrier_0.src = "/assets/airCarrier-0.png";
-    const airCarrier_1 = new Image();
-    airCarrier_1.src = "/assets/airCarrier-1.png";
-    const battleship_0 = new Image();
-    battleship_0.src = "/assets/battleship-0.png";
-    const battleship_1 = new Image();
-    battleship_1.src = "/assets/battleship-1.png";
-    const cruiser_0 = new Image();
-    cruiser_0.src = "/assets/cruiser-0.png";
-    const cruiser_1 = new Image();
-    cruiser_1.src = "/assets/cruiser-1.png";
-    const destroyer_0 = new Image();
-    destroyer_0.src = "/assets/destroyer-0.png";
-    const destroyer_1 = new Image();
-    destroyer_1.src = "/assets/destroyer-1.png";
-    const submarine_0 = new Image();
-    submarine_0.src = "/assets/submarine-0.png";
-    const submarine_1 = new Image();
-    submarine_1.src = "/assets/submarine-1.png";
 
-    const explosion = new Image();
-    explosion.src = "/assets/explosion.png";
-    const fog = new Image();
-    fog.src = "/assets/explosion.png";
-    const sea = new Image();
-    sea.src = "/assets/explosion.png";
-};
 
-function get_img(filename) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function get_img(filenamem, rotation) {
+    if (rotation == 0){
     switch (filename) {
         case "airCarrier-0.png":
-            return airCarrier_0;
+            return airCarrier_0_W;
         case "airCarrier-1.png":
-            return airCarrier_1;
+            return airCarrier_1_W;
         case "battleship-0.png":
-            return battleship_0;
+            return battleship_0_W;
         case "battleship-1.png":
-            return battleship_1;
+            return battleship_1_W;
         case "cruiser-0.png":
-            return cruiser_0;
+            return cruiser_0_W;
         case "cruiser-1.png":
-            return cruiser_1;
+            return cruiser_1_W;
         case "destroyer-0.png":
-            return destroyer_0;
+            return destroyer_0_W;
         case "destroyer-1.png":
-            return destroyer_1;
+            return destroyer_1_W;
         case "submarine-0.png":
-            return submarine_0;
+            return submarine_0_W;
         case "submarine-1.png":
-            return submarine_1;
+            return submarine_1_W;
         default:
             throw Error();
-    };
+    };} else if (rotation == 1)
+
+    {switch (filename) {
+        case "airCarrier-0.png":
+            return airCarrier_0_N;
+        case "airCarrier-1.png":
+            return airCarrier_1_N;
+        case "battleship-0.png":
+            return battleship_0_N;
+        case "battleship-1.png":
+            return battleship_1_N;
+        case "cruiser-0.png":
+            return cruiser_0_N;
+        case "cruiser-1.png":
+            return cruiser_1_N;
+        case "destroyer-0.png":
+            return destroyer_0_N;
+        case "destroyer-1.png":
+            return destroyer_1_N;
+        case "submarine-0.png":
+            return submarine_0_N;
+        case "submarine-1.png":
+            return submarine_1_N;
+        default:
+            throw Error();
+    };} else if (rotation == 2)
+
+    {switch (filename) {
+        case "airCarrier-0.png":
+            return airCarrier_0_E;
+        case "airCarrier-1.png":
+            return airCarrier_1_E;
+        case "battleship-0.png":
+            return battleship_0_E;
+        case "battleship-1.png":
+            return battleship_1_E;
+        case "cruiser-0.png":
+            return cruiser_0_E;
+        case "cruiser-1.png":
+            return cruiser_1_E;
+        case "destroyer-0.png":
+            return destroyer_0_E;
+        case "destroyer-1.png":
+            return destroyer_1_E;
+        case "submarine-0.png":
+            return submarine_0_E;
+        case "submarine-1.png":
+            return submarine_1_E;
+        default:
+            throw Error();
+    };} else 
+
+    {switch (filename) {
+        case "airCarrier-0.png":
+            return airCarrier_0_S;
+        case "airCarrier-1.png":
+            return airCarrier_1_S;
+        case "battleship-0.png":
+            return battleship_0_S;
+        case "battleship-1.png":
+            return battleship_1_S;
+        case "cruiser-0.png":
+            return cruiser_0_S;
+        case "cruiser-1.png":
+            return cruiser_1_S;
+        case "destroyer-0.png":
+            return destroyer_0_S;
+        case "destroyer-1.png":
+            return destroyer_1_S;
+        case "submarine-0.png":
+            return submarine_0_S;
+        case "submarine-1.png":
+            return submarine_1_S;
+        default:
+            throw Error();
+    };}
 };
 
 function coord_to_pos(coord, rotation) {
