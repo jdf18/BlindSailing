a351e446a10ddbf882f422af56d77aa1de8ea454
Michael Simm <179273005+mickyj0101@users.noreply.github.com>
2024-11-03 02:36:35 +0000

    Finally get up-to-date

--- src/server/ships.py
+++ src/server/ships.py
@@ -2,7 +2,7 @@ import numpy as np
 # Base class for all ships. front is the co-ords of the front of the ship, facing is the direction the ship is facing
 # Length is the length of the ship (all ships have width 1). viewRadius and fireRadius are the radiuses the ship can see and shoot in
 class Ship:
-    def __init__(self, length: int, front: np.array, facing: np.array, viewRadius: int, fireRadius: int, team: int):
+    def __init__(self, length: int, front: np.array, facing: np.array, viewRadius: int, fireRadius: int, team: int, id: str):
         self.length = length
         self.front = front
         self.facing = facing
@@ -13,6 +13,7 @@ class Ship:
             raise ValueError("team must be 1 or 2")
         self.team = team
         self.hitSections = [False for i in range(length)]
+        self.id = id
 
     def step(self, distance: int):
         self.front += distance * self.facing
@@ -40,29 +41,44 @@ class Ship:
     
     def isDead(self) -> bool:
         return all(self.hitSections)
+    
+    def getFacingasValue(self):
+        match self.facing:
+            case np.array([-1, 0]):
+                return 0
+            case np.array([0, -1]):
+                return 1
+            case np.array([1, 0]):
+                return 2
+            case np.array([0, 1]):
+                return 3
+        
+    def getDamagedCoords(self):
+        coords = self.getCoords()
+        return [coords[i] for i in range(len(coords)) if self.hitSections[i]]
 
 
 # 5-long
 class AirCarrier(Ship):
     def __init__(self, front: np.array, facing: np.array, team: int):
-        super().__init__(5, front, facing, 12, 6, team)
+        super().__init__(5, front, facing, 12, 6, team, "AirCarrier")
 
 # 4-long
 class Battleship(Ship):
     def __init__(self, front: np.array, facing: np.array, team: int):
-        super().__init__(4, front, facing, 8, 8, team)
+        super().__init__(4, front, facing, 8, 8, team, "Battleship")
 
 # 3-long
 class Cruiser(Ship):
     def __init__(self, front: np.array, facing: np.array, team: int):
-        super().__init__(3, front, facing, 6, 6, team)
+        super().__init__(3, front, facing, 6, 6, team, "Cruiser")
 
 # 3-long
 class Submarine(Ship):
     def __init__(self, front: np.array, facing: np.array, team: int):
-        super().__init__(3, front, facing, 5, 12, team)
+        super().__init__(3, front, facing, 5, 12, team, "Submarine")
 
 # 2-long
 class Destroyer(Ship):
     def __init__(self, front: np.array, facing: np.array, team: int):
-        super().__init__(2, front, facing, 4, 4, team)
+        super().__init__(2, front, facing, 4, 4, team, "Destroyer")
