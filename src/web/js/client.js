c10a665f6537bf0e41b8d5f94e8c1da5755118a6
jdf18 <97624626+jdf18@users.noreply.github.com>
2024-11-03 01:42:36 +0000

    User input activation/deactivation

---
+++ src/web/js/client.js
@@ -0,0 +1,9 @@
+is_user_input_activated = false;
+
+function activate_user_input() {
+    is_user_input_activated = true
+}
+
+function deactivate_user_input() {
+    is_user_input_activated = false
+}
